3d2c47994095ff32b251fc4d0c77339cb9761c4d
Marcin Magdziak <marcinm@live.nl>
2024-12-09 16:42:42 +0100

    stashing changes

--- main.js
+++ main.js
@@ -4,24 +4,46 @@
 */
 
 // Stap 1: selecteer het 12e linkje, en sla deze op in een variabele
-let interaction = document.querySelector('a:nth-of-type(12)')
+const parent = document.querySelector('section')
+const number = parent.childElementCount
+const allButtons = document.querySelectorAll("a");
 
 // Stap 2: voeg de (click) event listener toe aan de link, met een callback functie
-// In dit geval wordt de jumpHandler functie aangeroepen, zodra je op het linkje klikt
-interaction.addEventListener('click', jumpHandler)
 
 // Deze jumpHandler functie staat klaar voor als we 'm aanroepen; deze wordt dus
 // _niet_ meteen bij het laden van de pagina aangeroepen
-function jumpHandler() {
+const animationNames = ["bibber", "stepper", "sadness", "glowow", "joe", "joe", "joe", "joe", "joe", "joe", "joe", "jump"];
 
-  // Stap 3: toggle een class op het element (zodra er dus op een link wordt geklikt)
-  interaction.classList.toggle('jump')
-}
+// In dit geval wordt de jumpHandler functie aangeroepen, zodra je op het linkje klikt
+allButtons.forEach((button, i) => {
+  const eventType = i % 3 === 0 
+    ? "mouseover"
+    : i % 3 === 1
+    ? "click"
+    : "mouseleave";
+  button.addEventListener(eventType, () => {
+    button.classList.toggle(animationNames[i]);
+    console.log(`Triggered ${eventType} for button ${i}`);
+  })
+  button.addEventListener('animationiteration', () => {
 
-// Extraatje, waardoor de class weer weggehaald wordt zodra de animatie afgelopen is
-interaction.addEventListener('animationend', jumpHandler)
+  })
+  // Extraatje, waardoor de class weer weggehaald wordt zodra de animatie afgelopen is
+  button.addEventListener("animationend", () => {
+    button.classList.remove(animationNames[i]);
+  })
+  // console.log(button);
+})
+// interaction12.addEventListener('click', jumpHandler)
+let i = allButtons.length - 1
+document.addEventListener("keydown", (e) => {
+if(e.key === "Backspace") allButtons[i].remove();
+console.log(allButtons[i]);
+console.log(number);
 
 
+  
+})
 // Ga zelf verder met de overige elementen, aan de hand van de instructies
 // Maak bijvoorbeeld een bibber animatie als je op iets klikt
 
